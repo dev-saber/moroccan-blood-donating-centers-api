d1889d1a6c545df03b68acda545fb36618e8f133
sifeddineeddr <sifeddine.edr@gmail.com>
2023-07-15 23:23:42 +0100

    mongodb connection && loading env variables

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+from pymongo import MongoClient
 
 app = Flask(__name__)
+load_dotenv()
 
-@app.route('/')
+client = MongoClient(os.getenv("MONGODB_URI"))
+centers = client["data"]["centers"]
+
+
+@app.route("/")
 def home():
-    return 'home'
+    data = centers.find()
+    # return list(data)
+    # Convert ObjectId to string for serialization
+    serialized_data = []
+    for document in data:
+        document["_id"] = str(document["_id"])
+        serialized_data.append(document)
+    return jsonify(serialized_data)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
